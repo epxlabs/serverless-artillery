af3fdd647c48ac24afc6e4d90a8abd3d13c12e07
Plitzer <cameronrobertp@gmail.com>
2022-10-26 21:36:01 +1100

    fix

--- tests/lib/index.spec.js
+++ tests/lib/index.spec.js
@@ -1125,7 +1125,7 @@ scenarios:
           .then(slsart.impl.getScriptText)
           .then((yml) => {
             const sls = yaml.safeLoad(yml)
-            expect(sls.service).to.not.have.string('s')
+            expect(sls.service).to.not.have.string('_')
           })
           .should.be.fulfilled
       })
